cdfaef116bb791c218e149573eadfa7cc75fff69
nicoraww <94559409+nicoraww@users.noreply.github.com>
2026-02-04 17:11:54 -0500

    Delete streamlit.py

--- streamlit.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Adopción de Perros", layout="wide")
-
-with open("index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-st.components.v1.html(html, height=900, scrolling=True)
